9f9a4c3be043fa5ae19686a082be163a5939dc2e
mitchredd77 <147752683+mitchredd77@users.noreply.github.com>
2023-10-15 10:51:10 -0600

    Update Number_Guessing_Game.py

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -19,33 +19,32 @@ def start_game():
 ### While loop to ask for a number and evaluate the answer given
    while True:
        try:
-          num_guess = int(input("Try to guess the correct number in between 1 and 10 "))
+          num_guess = int(input("\nTry to guess the correct number in between 1 and 10 "))
           total_guesses += 1
           
 ### Handle guesses that are floats or non-integer
        except ValueError:
-          print("That is not an exact number of 1, 2, 3, 4, 5, 6, 7, 8, 9, or 10!")
+          print("\nThat is not an exact number of 1, 2, 3, 4, 5, 6, 7, 8, 9, or 10!")
           continue
          
 ### If there is any other exception, ask for a new guess
        except:
-          print("There was an error with your guess, please try again.")
+          print("\nThere was an error with your guess, please try again.")
           continue
           
 ### When answer is correct, add score to scores list, and ask if they would like to play again
        if num_guess == ran_num:
-            print("{} is the correct number!".format(num_guess))
-            print("It took you {} tries to get the correct number".format(total_guesses))
+            print("\n{} is the correct number!".format(num_guess))
+            print("\nIt took you {} tries to get the correct number".format(total_guesses))
             scores.append(total_guesses)
-            scores.sort()
-            best_score = scores[0]
-            play_again = input("Would you like to play again? (yes or no) ").lower()
+            best_score = min(scores)
+            play_again = input("\nWould you like to play again? (yes or no) ").lower()
             if play_again == "yes":
-                print("Your current lowest amount of guesses to get the correct number is {}".format(best_score))
+                print("\nYour current lowest amount of guesses to get the correct number is {}".format(best_score))
                 ran_num = random.randint(1, 10)
                 total_guesses = 0
             else:
-                print("Your best score was {} guesses. Goodbye and have a great day!".format(best_score))
+                print("\nYour best score was {} guesses. Goodbye and have a great day!".format(best_score))
                 return False
                
 ### If the number was less than 1 or greater than 10
